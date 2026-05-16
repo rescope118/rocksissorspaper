e4512a1c7ac9d94648118b3c0cd53efb1b9a4abc
unknown <jeh101108@gmail.com>
2026-05-16 17:06:49 +0900

    기능추가: 묵찌빠 기능 추가

--- rocksissorspaper.py
+++ rocksissorspaper.py
@@ -1,7 +1,50 @@
+def mukssippa(turn) :
+    while True :
+        while True:
+            player = input("묵찌빠 ")
+
+            if player == "찌" :
+                player_num = 1
+                break
+            elif player == "묵" :
+                player_num = 2
+                break
+            elif player == "빠":
+                player_num = 3 
+                break
+            else :
+                print("묵, 찌, 빠 중 하나를 입력하시오")
+        
+        random.randint(1, 3)
+        if computer_num == 2:
+            computer = "찌"
+        elif computer_num == 3:
+            computer = "묵"
+        else :
+            computer = "빠"
+            
+        print(f"플레이어:{player}")
+        print(f'컴퓨터  :{computer}')
+        if computer_num == player_num :
+            print('플레이어 턴')
+            turn = 1
+        elif computer_num - 1 == player_num or computer_num + 2 == player_num:
+            if turn == 1:
+                print('플레이어 승')
+                break
+            else :
+                print('컴퓨터 승')
+                break
+        else :
+            print('컴퓨터 턴')
+            turn = 2
+    
+turn = 0
+
 while True :
     player_num = 0
     while True:
-        player = input("가위, 바위, 보")
+        player = input("가위, 바위, 보 ")
 
         if player == "가위" :
             player_num = 1
@@ -29,10 +72,15 @@ while True :
     print(f"플레이어:{player}")
     print(f'컴퓨터  :{computer}')
     if computer_num == player_num :
-        print('플레이어 승')
-        break
+        print('플레이어 턴')
+        turn = 1
     elif computer_num - 1 == player_num or computer_num + 2 == player_num:
         print('무승부')
+        turn = 0
     else :
-        print('컴퓨터 승')
+        print('컴퓨터 턴')
+        turn = 2
+    
+    if turn != 0 :
+        mukssippa(turn)
         break
